4dcd8dbe79fa1b656bbfbc2b9b1f9d3a6074dd4e
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-26 10:45:27 +0100

    good
    done

--- src/MainBoard/MainBoard.jsx
+++ src/MainBoard/MainBoard.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./MainBoard.css";
 
-const MainBoard = () => {
+function WeightConverter() {
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+  const [result, setResult] = useState("");
+
+  const updateResults = () => {
+    const input = parseFloat(inputValue);
+    if (input <= 0 || isNaN(input)) {
+      setError("Please enter a valid number!");
+      setResult(""); // Clear the result when error occurs
+    } else {
+      setResult((input / 2.2).toFixed(2));
+      setError(""); // Clear error when valid input is provided
+    }
+  };
+
   return (
-    <div>MainBoard</div>
-  )
+    <div className="container">
+      <h1 className="heading">Weight Converter</h1>
+      <div className="input-container">
+        <label htmlFor="pounds">Pounds:</label>
+        <input
+          type="number"
+          id="input"
+          className="input"
+          step=".1"
+          placeholder="Enter number"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            updateResults(); // Call updateResults when the input changes
+          }}
+        />
+      </div>
+      <p>
+        Your weight in kg is: <span id="result">{result}</span>
+      </p>
+      <p className="error">{error}</p>
+    </div>
+  );
 }
 
-export default MainBoard
+export default WeightConverter;
